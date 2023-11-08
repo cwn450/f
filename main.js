5f8b8005752f53fc97119716b7af1db4fc0d5adf
cwn450 <118194942+cwn450@users.noreply.github.com>
2023-11-08 20:47:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+leftWristx=0
+leftWristy=0
+rightWristx=0
+rightWristy=0
+
+song=""
+function preload()
+{
+song=loadSound("music.mp3")
+}
+
+
+
+
+
+function setup()
+{
+    canvas=createCanvas(600,500)
+    canvas.center()
+
+    video=createCapture(VIDEO)
+    video.hide()
+    poseNet=ml5.poseNet(video,modelLoaded)
+    poseNet.on('pose', gotposes)
+
+}
+
+function draw()
+{
+    image(video,0,0.600,500)
+}
+function play()
+{
+    song.play()
+    song.setVolume(1)
+    song.rate(1)
+}
+
+function modelLoaded(){
+    console.log('posenet is initialized')
+}
+
+function gotposes(results){
+    if(results.length>0){
+        console.log(results)
+        leftWristx=results[0].pose.leftWrist.x
+        leftWristy=results[0].pose.leftWrist.y
+        console.log("leftWristx="+leftWristx+"leftWristy="+leftWristy)
+
+        rightWristx=results[0].pose.rightWrist.x
+        rightWristy=results[0].pose.rightWrist.y
+        console.log("rightWistx="+rightWristx+"rightWristy="+rightWristy)
+    }
+}
